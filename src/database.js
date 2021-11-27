a753e276a88f0b6dc738ac91e94d87c7c7e90181
ArgueraDev <robertoarguera@gmail.com>
2021-11-26 18:40:56 -0600

    ...

--- src/database.js
+++ src/database.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mogodb://localhost/mydatabase')
+mongoose.connect('mogodb://localhost/mydatabase',{
+    useNewUrlParse: true,
+    useUnifiedTopology: true
+})
 .then(db => console.log('db conectada', db.connection.host))
 .catch(err => console.error(err));
