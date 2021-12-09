1dc118dbb6b6fe6fbe7b700c24080658ba3527d2
xMichaelRodriguez <scottlovos503@gmail.com>
2021-12-09 11:58:50 -0600

    PARCIAL 3

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,17 @@
+const express = require('express')
+const serveIndex = require('serve-index')
+const path = require('path')
+const sgMail = require('@sendgrid/mail');
+
 const {
     Router
 } = require('express');
 const router = Router();
+
+const key = "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";
+sgMail.setApiKey("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");
 const User = require('../models/user');
+
 router.get('/', (req, res) => {
     User.find((err, users) => {
         if (err) {
@@ -60,4 +69,40 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', async (req, res) => {
+
+    const {mail} = req.body;
+console.log(mail)
+    const msg = {
+        to: mail,
+        from: 'michael5031rodriguez@gmail.com', // Use the email address or domain you verified above
+        subject: 'Enviando mensaje desde servidor de:',
+        text: 'parcial numero 3 sorftware libre',
+        html: `
+            <strong>
+                <ul>
+                    <li>Michael Scott Lovo Rodriguez USIS013818</li>
+                    <li>Melissa Estefania Diaz Orellana USIS038918</li>
+                    <li>Laura Maria Rivas Guerrero USIS053818</li>
+                    <li>Jimmy Edgardo Martinez USIS000718</li>
+                    <li>Roberto Carlos Arguera Campos USIS008718</li>
+                    </ul>
+            </strong>`,
+    };
+
+    try {
+        await sgMail.send(msg);
+        res.redirect('/')
+    } catch (error) {
+        if (error.response) {
+            console.error(error.response.body)
+        }
+        console.log(error)
+    }
+
+});
+
+router.use('/ftp-route', express.static(path.join(process.cwd(), '/src/public')), serveIndex(path.join(process.cwd(), '/src/public'), { icons: true }));
+
 module.exports = router;
+console.log(path.join(process.cwd(), '/src/public'))
